d991bc744d83085a64cf7b127070cb652dd441b1
WW <makarena@gmail.com>
2026-01-21 16:26:35 +0100

    21.01

--- formularze/01_szkola/uczniowie.php
+++ formularze/01_szkola/uczniowie.php
@@ -1,11 +1,52 @@
+<?php
+$link = new mysqli('localhost','root','','szkola');
+$sql = "SELECT * FROM uczen;";
+$result = $link -> query($sql);
+$students = $result -> fetch_all(1);
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        th,td{
+            border:1px solid black;
+        }
+        table{
+            border-collapse:collapse;
+        }
+    </style>
 </head>
 <body>
-    
+    <h1>Uczniowie</h1>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>imie</th>
+            <th>nazwisko</th>
+        </tr>
+        <!-- <tr>
+            <td> [id] </td>
+            <td> [imie] </td>
+            <td> [nazwisko] </td>
+        </tr> -->
+        <?php
+        foreach($students as $student){
+            echo "<tr>
+                  <td> {$student['id']} </td>
+                  <td> {$student['imie']} </td>
+                  <td> {$student['nazwisko']} </td>
+                  </tr>";
+        }
+        
+        ?>
+    </table>
 </body>
 </html>
+
+
+<?php
+$link -> close();
+?>
