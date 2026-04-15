abcbfcd050962b850aef02f3b2838179b77d6de1
WW <makarena@gmail.com>
2026-04-15 15:58:53 +0200

    15.04.2026

--- formularze/05_biblioteka/05_biblioteka.php
+++ formularze/05_biblioteka/05_biblioteka.php
@@ -4,10 +4,30 @@ $name_f = $_POST['name_f']??NULL;
 $surname_f = $_POST['surname_f']??NULL;
 if($name_f && $surname_f){
     $sql = "INSERT INTO czytelnicy
-VALUES
-(NULL,'$name_f','$surname_f','');";
-$link -> query($sql);
+    VALUES
+    (NULL,'$name_f','$surname_f','');";
+    $link -> query($sql);
 }
+
+$book_id_f=$_POST['book-id']??NULL;
+if($book_id_f){
+    $sql="DELETE FROM ksiazki
+        WHERE id=$book_id_f;";
+    $link -> query($sql);
+}
+
+$sql="SELECT ksiazki.id,ksiazki.tytul,autorzy.imie,autorzy.nazwisko
+    FROM ksiazki INNER JOIN autorzy ON ksiazki.id_autor=autorzy.id;";
+$result=$link -> query($sql);
+$books=$result -> fetch_all(1);
+
+$sql="SELECT wypozyczenia.id,ksiazki.tytul,czytelnicy.nazwisko,wypozyczenia.data_oddania
+    FROM wypozyczenia 
+    INNER JOIN ksiazki ON wypozyczenia.id_ksiazka=ksiazki.id 
+    INNER JOIN czytelnicy ON wypozyczenia.id_czytelnik=czytelnicy.id;";
+$result=$link -> query($sql);
+$rentals=$result -> fetch_all(1);
+// var_dump($result)
 ?>
 
 <!DOCTYPE html>
@@ -28,12 +48,17 @@ $link -> query($sql);
     </form>
 
     <h2>Usuwanie książek</h2>
-    <ol>
-    <!-- skrypt b -->
-    </ol>
+    <ul>
+        <!-- <li>[id] [tytul] - [imie] [nazwisko]</li> -->
+         <?php
+         foreach($books as $book){
+            echo "<li>{$book['id']} {$book['tytul']} - {$book['imie']} {$book['nazwisko']}</li>";
+         }
+         ?>
+    </ul>
     <form action="" method="post">
-        <label for="book_id">Id książki</label>
-        <input type="number" name="book_id_f" id="book_id">
+        <label for="book-id">Id książki</label>
+        <input type="number" name="book-id" id="book-id">
         <button>Wyślij</button>
     </form>
 
@@ -45,7 +70,23 @@ $link -> query($sql);
             <th>naziwsko czytelnika</th>
             <th>data oddania</th>
         </tr>
-        <!-- skrypt d -->
+        <!-- <tr>
+            <td>[id]</td>
+            <td>[tytul]</td>
+            <td>[nazwisko]</td>
+            <td>[data_oddania]</td>
+        </tr> -->
+        <?php
+        foreach($rentals as $rental){
+            echo "
+            <tr>
+                <td>{$rental['id']}</td>
+                <td>{$rental['tytul']}</td>
+                <td>{$rental['nazwisko']}</td>
+                <td>{$rental['data_oddania']}</td>
+            </tr>";
+        }
+        ?>
     </table>
     <form action="" method="post">
         <label for="borrowed_id">Id wypozyczenia</label>
